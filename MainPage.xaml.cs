f95b91dbf2872f3ac2ed6d265e39ce052f23ed7e
hansf14 <sam_h0117@hotmail.com>
2024-09-05 09:02:25 +0900

    Sliders, copy to clipboard button, randomize color button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,69 @@
-﻿namespace maui_color_maker
+﻿using CommunityToolkit.Maui.Alerts;
+using System.Diagnostics;
+
+namespace maui_color_maker
 {
   public partial class MainPage : ContentPage
   {
+    bool isRandom;
+    string hexValue;
+
     public MainPage()
     {
       InitializeComponent();
+      isRandom = false;
+      hexValue = "#000000";
+    }
+
+    private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+      if (isRandom)
+      {
+        return;
+      }
+
+      var red = sliderRed.Value;
+      var green = sliderGreen.Value;
+      var blue = sliderBlue.Value;
+
+      Color color = Color.FromRgb(red, green, blue);
+      SetColor(color);
+    }
+
+    private void SetColor(Color color)
+    {
+      Debug.WriteLine(color.ToString());
+      buttonRandom.BackgroundColor = color;
+      Container.BackgroundColor = color;
+      hexValue = color.ToHex();
+      labelHex.Text = hexValue;
+    }
+
+    private void ButtonRandom_Clicked(object sender, EventArgs e)
+    {
+      isRandom = true;
+
+      var random = new Random();
+      var color = Color.FromRgb(
+        random.Next(0, 256),
+        random.Next(0, 256),
+        random.Next(0, 256)
+      );
+      SetColor(color);
+
+      sliderRed.Value = color.Red;
+      sliderGreen.Value = color.Green;
+      sliderBlue.Value = color.Blue;
+
+      isRandom = false;
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+      await Clipboard.SetTextAsync(hexValue);
+
+      var toast = Toast.Make("Color Copied", CommunityToolkit.Maui.Core.ToastDuration.Short, 12);
+      await toast.Show();
     }
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# maui-color-maker
+# Maui Practice - Maui Todo List
+
+- **One of my personal digital nomad life projects (for practice).**
+- **Project current status: [complete]**:ballot_box_with_check:
+- **Tech**
+<p>
+  <div>
+    &emsp;
+    <img src="https://img.shields.io/badge/C%23-239120?style=flat&logo=c-sharp&logoColor=white&logoWidth=25" height="25px"/>
+  </div>
+  <div>
+    &emsp;
+    <img src="https://img.shields.io/badge/.NET MAUI-512BD4?style=flat&logo=maui&logoColor=white&logoWidth=25" height="25px"/>
+    <img src="https://img.shields.io/badge/CommunityToolkit.Maui-512BD4?style=flat&logo=maui&logoColor=white&logoWidth=25" height="25px"/>
+  </div>
+</p>
+
+- **Preview Screenshots**
+<p>
+  <div>
+    &emsp;
+    <img width="300px" src="PreviewScreenshots/maui-color-maker-01.png" alt="maui-color-maker-01.png" />
+  </div>
+  &emsp;
+  <div>
+    &emsp;
+    <img width="300px" src="PreviewScreenshots/maui-color-maker-02.png" alt="maui-color-maker-02.png" />
+  </div>
+  &emsp;
+  <div>
+    &emsp;
+    <img width="300px" src="PreviewScreenshots/maui-color-maker-03.png" alt="maui-color-maker-03.png" />
+  </div>
+  &emsp;
+  <div>
+    &emsp;
+    <img width="300px" src="PreviewScreenshots/maui-color-maker-04.png" alt="maui-color-maker-04.png" />
+  </div>
+</p>
+
+<br/>
